5bae4f362e2600d75687d0c66e04712b46ec3607
Your Name <you@example.com>
2026-08-29 15:10:41 -0400

    Primer commit y subida de archivos iniciales

---
+++ MiPetSaludApp/src/test/java/com/MiPetSalud/MiPetSaludApp/MiPetSaludAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MiPetSalud.MiPetSaludApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiPetSaludAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
