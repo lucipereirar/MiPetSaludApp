0f330a93960a9f6b8e0a2718d40bc1dedb7c4992
Your Name <you@example.com>
2026-09-01 23:51:23 -0400

    Construccion de entidad Mascota

--- MiPetSaludApp/src/main/java/com/MiPetSalud/MiPetSaludApp/domain/mascota/MascotaEntity.java
+++ MiPetSaludApp/src/main/java/com/MiPetSalud/MiPetSaludApp/domain/mascota/MascotaEntity.java
@@ -1,4 +1,38 @@
 package com.MiPetSalud.MiPetSaludApp.domain.mascota;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+@Entity
+@Table(name="Mascota")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+//Estructura de la clase Mascota
 public class MascotaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false)
+    private String usuarioId;
+
+    @Column(nullable = false)
+    private String nombreMascota;
+
+    @Column(nullable = false)
+    private String genero;
+
+    @Column(nullable = false)
+    private String raza;
+
+    @Column(nullable = false)
+    private LocalDate fecNacimiento;
 }
